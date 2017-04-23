b22bc054220cc70155bb9e9a3f33820dbb5b20da
游由 <huang_xianjun@live.cn>
2017-04-23 23:26:29 +0800

    Create helloWorld.cs
    C#

---
+++ helloWorld.cs
@@ -0,0 +1,10 @@
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
